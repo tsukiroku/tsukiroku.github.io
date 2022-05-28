6b524f958a6afb39965c5a8d7469f1168e1bc716
tsukiroku <tsukiroku@yahoo.com>
2022-05-28 17:00:18 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,5 @@
 <script lang="ts">
-    import Main from './Main.svelte';
+    import Title from './title.svelte';
 </script>
 
-<img
-    src="https://cdn.discordapp.com/emojis/786105779832750080.png"
-    class="ani"
-    alt=""
-/>
-
-<Main />
-<Main smRlavy />
-
-<style>
-    img.ani {
-        position: fixed;
-        height: 10vh;
-        width: auto;
-        right: 0;
-        bottom: 0;
-    }
-</style>
+<Title />
