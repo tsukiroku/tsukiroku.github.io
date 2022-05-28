afd64ec630005debad2d3bf8474a3dc301c391eb
tsukiroku <tsukiroku@yahoo.com>
2022-05-28 21:35:09 +0900

    Update themeScheme.ts

--- src/themeScheme.ts
+++ src/themeScheme.ts
@@ -3,10 +3,12 @@ export default {
         background: '#fafafa',
         default: '#000000',
         description: '#3d3d3d',
+        nextText: '#4a5ef7',
     },
     dark: {
         background: '#151515',
         default: '#ffffff',
         description: '#dbdbdb',
+        nextText: '#fad20c',
     },
 }[window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'];
