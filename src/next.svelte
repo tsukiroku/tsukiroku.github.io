bd62299992bfd7b668916c7ce545006f942179ac
tsukiroku <tsukiroku@yahoo.com>
2022-05-28 21:35:07 +0900

    Create next.svelte

---
+++ src/next.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import theme from './themeScheme';
+</script>
+
+<p class="next" style="color: {theme.nextText}">Projects -></p>
+
+<style>
+    p.next {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        padding: 15px 35px;
+        font-size: 20px;
+        font-family: 'Fira Code', sans-serif;
+    }
+</style>
