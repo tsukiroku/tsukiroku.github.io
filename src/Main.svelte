ad8eedf4fda19444dfe66c09ddf453e284934e3c
tsukiroku <tsukiroku@yahoo.com>
2022-05-28 16:20:19 +0900

    Update Main.svelte

--- src/Main.svelte
+++ src/Main.svelte
@@ -22,13 +22,47 @@
     div.container > h1 {
         color: #ff3e00;
         text-transform: uppercase;
-        font-size: 4em;
+        font-size: 80px;
         font-weight: 100;
+        animation-name: bounce;
+        animation-duration: 1s;
+    }
+
+    div.container > p {
+        font-size: 25px;
     }
 
     @media (min-width: 640px) {
         div.container {
             max-width: none;
+        }
+    }
+
+    @media (max-width: 640px) {
+        div.container > h1 {
+            font-size: 10vh;
+        }
+
+        div.container > p {
+            font-size: 5vh;
+        }
+    }
+
+    @keyframes bounce {
+        0%,
+        20%,
+        50%,
+        80%,
+        100% {
+            transform: translateY(0);
+        }
+
+        40% {
+            transform: translateY(-30px);
+        }
+
+        60% {
+            transform: translateY(-15px);
         }
     }
 </style>
