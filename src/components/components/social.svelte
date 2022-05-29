dc624b845d079bab05abd24d987b3a41b6b5b225
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 13:49:45 +0900

    Create social.svelte

---
+++ src/components/components/social.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let name: string;
+    export let url: string;
+    export let color: string;
+</script>
+
+<div class="social">
+    <a href={url}>
+        <p class="name" style="color: {color}" id={`social-${name}`}>
+            {name}
+        </p>
+    </a>
+    <br />
+</div>
+
+<style>
+    * {
+        padding-bottom: 20px;
+        font-size: 20px;
+    }
+
+    a:link,
+    a:visited,
+    a:hover,
+    a:active {
+        text-decoration: none;
+    }
+
+    .social > * > .name {
+        font-weight: bold;
+        display: inline;
+    }
+
+    @media (max-width: 640px) {
+        * {
+            display: block;
+            font-size: 12px;
+            padding-bottom: 5px;
+        }
+
+        .social > * > .name {
+            font-size: 15px;
+        }
+    }
+</style>
