f64d2d6986482858756be21fbf51f371000c350c
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 11:53:43 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Title from './components/title.svelte';
     import Next from './components/github.svelte';
+    import Projects from './components/projects.svelte';
 </script>
 
 <Title />
 <Next />
+<Projects />
