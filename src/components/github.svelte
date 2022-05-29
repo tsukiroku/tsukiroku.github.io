438e8625cc50bac2f3cd337b9eb7ea6056f58bae
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 11:18:24 +0900

    Create github.svelte

---
+++ src/components/github.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import theme from '../themeScheme';
+</script>
+
+<p
+    class="next"
+    style="color: {theme.nextText}"
+    on:click={() => (location.href = 'https://github.com/tsukiroku')}
+>
+    Github ->
+</p>
+
+<style>
+    p.next {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        padding: 15px 35px;
+        font-size: 20px;
+        font-family: 'Fira Code', sans-serif;
+        cursor: pointer;
+    }
+</style>
