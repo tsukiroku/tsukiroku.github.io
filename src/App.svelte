f93c4332dd219bc7ffae3552de4a1239032648dd
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 13:49:43 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
     import Title from './components/title.svelte';
     import Next from './components/github.svelte';
     import Projects from './components/projects.svelte';
+    import Socials from './components/socials.svelte';
 </script>
 
 <Title />
 <Next />
 <Projects />
+<Socials />
