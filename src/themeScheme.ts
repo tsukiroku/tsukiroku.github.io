0625974ce8fae18a4ff67a39e8cedd719a5454dd
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 12:48:46 +0900

    Update themeScheme.ts

--- src/themeScheme.ts
+++ src/themeScheme.ts
@@ -4,11 +4,15 @@ export default {
         default: '#000000',
         description: '#3d3d3d',
         nextText: '#4a5ef7',
+        projectName: '#121212',
+        projectDescription: '#303030',
     },
     dark: {
         background: '#151515',
         default: '#ffffff',
         description: '#dbdbdb',
         nextText: '#fad20c',
+        projectName: '#e8e8e8',
+        projectDescription: '#c9c9c9',
     },
 }[window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'];
