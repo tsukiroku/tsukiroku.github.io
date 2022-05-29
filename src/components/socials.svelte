22c099b26ad7168b6b61975a7b477d49f1cc7c5a
tsukiroku <tsukiroku@yahoo.com>
2022-05-29 13:49:52 +0900

    Create socials.svelte

---
+++ src/components/socials.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import Social from './components/social.svelte';
+    import theme from '../themeScheme';
+    import { github } from '../main';
+</script>
+
+<div class="socials">
+    <p class="title" style="color: {theme.default}" id="socials">Socials</p>
+    <Social
+        name="Github"
+        url={`https://github.com/tsukiroku`}
+        color={theme.socialGithub}
+    />
+    <Social
+        name="Twitter"
+        url={`https://twitter.com/tsukiroku_t`}
+        color={theme.socialTwitter}
+    />
+</div>
+
+<style>
+    .socials {
+        position: absolute;
+        top: 200%;
+        right: 0;
+        padding: 100px 100px;
+        text-align: right;
+    }
+
+    .socials > .title {
+        font-size: 40px;
+        font-weight: bold;
+    }
+
+    @media (max-width: 640px) {
+        .socials {
+            text-align: center;
+            max-width: 240px;
+            margin: auto;
+            position: relative;
+            top: 40%;
+        }
+
+        .socials > .title {
+            font-size: 30px;
+            font-family: 'Fira Code', sans-serif;
+        }
+    }
+</style>
