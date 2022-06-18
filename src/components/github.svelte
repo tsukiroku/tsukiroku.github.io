36b3af244ee3f618f69e9e3700a53ee4bd74265c
tsukiroku <tsukiroku@yahoo.com>
2022-06-18 22:37:44 +0900

    Update github.svelte

--- src/components/github.svelte
+++ src/components/github.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
     import theme from '../themeScheme';
     import { github } from '../main';
-
-    const over = () => {
-        const element = document.getElementsByClassName(
-            'github'
-        )[0] as HTMLElement;
-        element.style.animationName = 'none';
-        requestAnimationFrame(() =>
-            setTimeout(() => (element.style.animationName = ''), 0)
-        );
-    };
 </script>
 
 <p
     class="github"
     style="color: {theme.nextText};"
     on:click={() => (location.href = github)}
-    on:focus={over}
 >
     Github ->
 </p>
@@ -31,29 +20,5 @@
         font-size: 20px;
         cursor: pointer;
         animation: shake_w 1s infinite both ease-in-out;
-    }
-
-    @keyframes shake_w {
-        0% {
-            transform: translateX(0);
-        }
-        6.5% {
-            transform: translateX(-2px) rotate(2deg);
-        }
-        18.5% {
-            transform: translateX(2px) rotate(2deg);
-        }
-        31.5% {
-            transform: translateX(-1px) rotate(-1deg);
-        }
-        43.5% {
-            transform: translateX(1px) rotate(1deg);
-        }
-        50% {
-            transform: translateX(0);
-        }
-        100% {
-            transform: none;
-        }
     }
 </style>
