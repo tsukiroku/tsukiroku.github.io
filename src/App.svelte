96379450fe8f2cfa5add0dfa70fd99bb20ef6b81
tsukiroku <tsukiroku@yahoo.com>
2022-07-31 15:29:34 +0900

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,16 @@
-<h1 class="text-3xl font-bold underline">
-    test
-</h1>
+<script>
+    import Title from './components/title.svelte';
+</script>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+
+    * {
+        background-color: #24292e;
+        font-family: 'Noto Sans', sans-serif;
+    }
+</style>
+
+<Title />
---
+++ src/components/title.svelte
@@ -0,0 +1,8 @@
+<div class="flex justify-center items-center h-screen">
+    <p class="text-white text-7xl font-bold">Hello</p>
+    <img
+        class="w-24 h-24 pl-5"
+        src="https://twemoji.maxcdn.com/v/latest/svg/1f44b.svg"
+        alt="twemoji"
+    />
+</div>
